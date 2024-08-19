ebf347bc4faf2d8d048d56507959fe9047a0b3a6
tikaya <tatiana.chefox@gmail.com>
2024-08-19 11:32:18 +0300

    adding main page route

--- src/main.js
+++ src/main.js
@@ -7,9 +7,21 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from './components/MainPage.vue'
+
 const vuetify = createVuetify({
     components,
     directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+const routes = [
+    { name: 'Home', path: '/', component: MainPage },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
