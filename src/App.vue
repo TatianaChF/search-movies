3bd4ea0b8991d9dd49433e7126435bdd56aeccdb
tikaya <tatiana.chefox@gmail.com>
2024-08-19 11:26:04 +0300

    adding a header logo and a button to change the theme color

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-container class="header">
+    <h1>MOVIES</h1>
+    <v-btn @click="toggleTheme">Изменить тему</v-btn>
+  </v-container>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
