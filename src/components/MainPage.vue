f9c8ca6f40198dcef1d56e75298280adedd56f0a
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 16:25:00 +0300

    output movies in line with hyphenation

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,13 @@
 <template>
-    <v-container v-for="movie in moviesList" :key="movie">
-        <v-card width="250">
-            <v-img :src="movie.poster.url" width="250" />
-            <v-card>
-                <h3 class="text">{{movie.name}}</h3>
+    <v-container class="container">
+        <v-container class="container__card" v-for="movie in moviesList" :key="movie">
+            <v-card width="250">
+                <v-img :src="movie.poster.url" width="250" />
+                <v-card>
+                    <h3 class="text">{{movie.name}}</h3>
+                </v-card>
             </v-card>
-        </v-card>
+        </v-container>
     </v-container>
 </template>
 
@@ -17,6 +19,15 @@ const moviesList = ref(moviesData.docs);
 </script>
 
 <style lang="scss" scoped>
+.container {
+    display: flex;
+    flex-wrap: wrap;
+
+    &__card {
+        flex: 250px;
+    }
+}
+
 .text {
     text-align: center;
 }
