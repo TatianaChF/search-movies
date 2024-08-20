5ca8e24f4b7ad3475689fa77f876312f6dbe4926
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 18:13:45 +0300

    pinia connecting

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import * as directives from 'vuetify/directives'
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from './components/MainPage.vue'
 import MovieCard from './components/MovieCard.vue'
+import { createPinia } from 'pinia'
 
 const vuetify = createVuetify({
     components,
@@ -26,4 +27,6 @@ const router = createRouter({
     routes,
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
