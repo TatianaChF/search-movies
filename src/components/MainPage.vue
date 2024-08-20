41aab2d30b2d328069cdd078b42757c69bc8cd5b
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 15:46:39 +0300

    output movies cards

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,19 @@
 <template>
-    <v-container>
-        Список фильмов
+    <v-container v-for="movie in moviesList" :key="movie">
+        <v-card width="350" height="700">
+            <v-img :src="movie.poster.url" />
+            <v-card>
+                {{movie.name}}
+            </v-card>
+        </v-card>
     </v-container>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import moviesData from "./../api/kinopoisk-1.json"
+
+const moviesList = ref(moviesData.docs);
+
+console.log(moviesList.value);
+</script>
