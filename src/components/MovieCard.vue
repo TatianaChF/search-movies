1deb844a8c9270d28abd018aa11c6df37a67c72e
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 20:10:29 +0300

    movie content text alignment

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-container class="container-movie-card">
-    <v-card width="865">
-    <v-img :src="movieData.poster.url" width="200" />
-    <v-container>
-      <h3>{{movieData.name}}</h3>
-      <p>Год выпуска: {{movieData.year}}</p>
-      <p>{{movieData.description}}</p>
-      <p>Длительность фильма: {{movieData.movieLength}} минут</p>
-      <p>Рейтинг: {{movieData.rating.kp}}</p>
-    </v-container>
-  </v-card>
+  <v-container class="container-movie">
+    <v-card class="container-movie__card" width="865">
+      <v-img :src="movieData.poster.url" width="200" />
+      <v-container class="container-movie__text">
+        <h3>{{movieData.name}}</h3>
+        <p>Год выпуска: {{movieData.year}}</p>
+        <p>{{movieData.description}}</p>
+        <p>Длительность фильма: {{movieData.movieLength}} минут</p>
+        <p>Рейтинг: {{movieData.rating.kp}}</p>
+      </v-container>
+    </v-card>
   </v-container>
 </template>
 
@@ -25,7 +25,15 @@ const movieData = moviesStore.movies.find((value) => value.name === route.params
 </script>
 
 <style lang="scss" scoped>
-.container-movie-card {
+.container-movie {
   text-align: center;
+
+  &__card {
+    display: flex;
+  }
+
+  &__text {
+    text-align: left;
+  }
 }
 </style>
