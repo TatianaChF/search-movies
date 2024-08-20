be34177d51d69ea4d41938a7eb645eecb1e2bd1c
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 18:15:03 +0300

    movies output from the movies store

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="container">
-        <v-container class="container__card" v-for="movie in moviesList" :key="movie">
+        <v-container class="container__card" v-for="movie in moviesStore.movies" :key="movie">
             <router-link :to="movie.name">
                 <v-card width="250">
                     <v-img :src="movie.poster.url" 
@@ -15,10 +15,9 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import moviesData from "./../api/kinopoisk-1.json";
+import { useMoviesStore } from "../store/movies";
 
-const moviesList = ref(moviesData.docs);
+const moviesStore = useMoviesStore();
 </script>
 
 <style lang="scss" scoped>
