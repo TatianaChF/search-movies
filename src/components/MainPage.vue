742db0f469b04f569135fe4f52a58bdd3156a369
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 16:36:39 +0300

    update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,7 +2,8 @@
     <v-container class="container">
         <v-container class="container__card" v-for="movie in moviesList" :key="movie">
             <v-card width="250">
-                <v-img :src="movie.poster.url" width="250" />
+                <v-img :src="movie.poster.url" 
+                        width="250" />
                 <v-card>
                     <h3 class="text">{{movie.name}}</h3>
                 </v-card>
