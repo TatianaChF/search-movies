a0865ad660c1274db604f5300c312890d60a646f
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 18:51:46 +0300

    display data about a certain movie

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>карточка фильма {{ movie.name }}</h2>
+</template>
+
+<script setup>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useMoviesStore } from "../store/movies";
+
+const moviesStore = useMoviesStore();
+const route = useRoute();
+const movie = moviesStore.movies.find((value) => value.name === route.params.name);
+
+console.log(movie);
+
+watch(
+  () => route.params.id,
+  (newId, oldId) => {
+    console.log(newId)
+  }
+)
+</script>
