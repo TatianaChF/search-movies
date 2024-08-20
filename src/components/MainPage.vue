5738565a0a74d99dd31999b4d30b2421b9f774a3
TatianaChF <tatiana.chefox.gmail.com>
2024-08-20 16:02:28 +0300

    movie name alignment

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,9 +1,9 @@
 <template>
     <v-container v-for="movie in moviesList" :key="movie">
-        <v-card width="350" height="700">
-            <v-img :src="movie.poster.url" />
+        <v-card width="250">
+            <v-img :src="movie.poster.url" width="250" />
             <v-card>
-                {{movie.name}}
+                <h3 class="text">{{movie.name}}</h3>
             </v-card>
         </v-card>
     </v-container>
@@ -14,6 +14,10 @@ import { onMounted, ref } from "vue";
 import moviesData from "./../api/kinopoisk-1.json"
 
 const moviesList = ref(moviesData.docs);
-
-console.log(moviesList.value);
 </script>
+
+<style lang="scss" scoped>
+.text {
+    text-align: center;
+}
+</style>
