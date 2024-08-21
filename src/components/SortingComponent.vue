cd33b8bebaf3c4a07730099aa20872efb7103694
tikaya <tatiana.chefox@gmail.com>
2024-08-21 10:14:00 +0300

    display a list of sorting options on click

--- src/components/SortingComponent.vue
+++ src/components/SortingComponent.vue
@@ -1,26 +1,46 @@
 <template>
-    <b>Сортировать по: </b>
-    <v-container>
-        <v-list>
-            <v-list-item v-for="sort in sortingValue" :key="sort">
-                {{ sort.title }}
-            </v-list-item>
-        </v-list>
+  <v-container>
+    <b>Сортировать по: <span @click="isOpen = !isOpen">{{ currentSortValue.title }}</span> </b>
+    <v-container class="container-list" v-if="isOpen">
+      <v-list width="300">
+        <v-list-item v-for="sort in sortingValue" :key="sort">
+          {{ sort.title }}
+        </v-list-item>
+      </v-list>
     </v-container>
+  </v-container>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const sortingValue = [
-    {name: "yearASC", sort: "year", title: "по году выпуска (по возрастанию)"},
-    {name: "yearDESC", sort: "-year", title: "по году выпуска (по убыванию)"},
-    {name: "populASC", sort: "rating", title: "по рейтингу (по возрастанию)"},
-    {name: "populDESC", sort: "-rating", title: "по рейтингу (по убыванию)"},
-    {name: "lengthASC", sort: "length", title: "по длительности (по возрастанию)"},
-    {name: "lengthDESC", sort: "-length", title: "по длительности (по убыванию)"}
+  { name: "yearASC", sort: "year", title: "по году выпуска (по возрастанию)" },
+  { name: "yearDESC", sort: "-year", title: "по году выпуска (по убыванию)" },
+  { name: "populASC", sort: "rating", title: "по рейтингу (по возрастанию)" },
+  { name: "populDESC", sort: "-rating", title: "по рейтингу (по убыванию)" },
+  {
+    name: "lengthASC",
+    sort: "length",
+    title: "по длительности (по возрастанию)",
+  },
+  {
+    name: "lengthDESC",
+    sort: "-length",
+    title: "по длительности (по убыванию)",
+  },
 ];
 
 let isOpen = ref(false);
-let currentSortValue = ref()
+let currentSortValue = ref({
+  name: "yearASC",
+  sort: "year",
+  title: "по году выпуска (по возрастанию)",
+});
 </script>
+
+<style lang="scss" scoped>
+.container-list {
+    margin-left: 120px;
+}
+</style>
