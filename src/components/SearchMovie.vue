7530c914caf1ca6985c485c876a7a149bd4b6bba
tikaya <tatiana.chefox@gmail.com>
2024-08-22 13:59:17 +0300

    adding search string value and watch for search string

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -1,7 +1,20 @@
 <template>
-    <v-responsive max-width="700">
-        <v-text-field label="Найти фильм" append-inner-icon="mdi-magnify" variant="underlined"/>
-    </v-responsive>
+  <v-responsive max-width="700">
+    <v-text-field
+      label="Найти фильм"
+      append-inner-icon="mdi-magnify"
+      variant="underlined"
+      v-model="searchValue"
+    />
+  </v-responsive>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, watch } from "vue";
+
+const searchValue = ref("");
+
+watch(searchValue, () => {
+    console.log(searchValue.value)
+})
+</script>
