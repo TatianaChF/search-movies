1d69034b979ed4f6f67fd659cd8e941fb6bf721d
tikaya <tatiana.chefox@gmail.com>
2024-08-22 13:47:37 +0300

    adding a search field

---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-responsive max-width="700">
+        <v-text-field label="Найти фильм" append-inner-icon="mdi-magnify" variant="underlined"/>
+    </v-responsive>
+</template>
+
+<script setup></script>
