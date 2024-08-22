342649bfb45607cc9d9d3fc59efec326087d014b
tikaya <tatiana.chefox@gmail.com>
2024-08-22 15:15:21 +0300

    changing the initial list array during search

--- src/store/movies.js
+++ src/store/movies.js
@@ -31,9 +31,7 @@ export const useMoviesStore = defineStore('moviesData', () => {
     }
 
     const searchMovie = (movieName) => {
-        if (movieName === "") {
-            movies.value = moviesData.docs;
-        }
+        movies.value = moviesData.docs;
 
         movies.value = movies.value.filter((movie) => {
             return movie.name.toLowerCase().includes(movieName);
