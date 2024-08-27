a8a3719a46bac5ce71aaac1944e6614654289924
TatianaChF <tatiana.chefox.gmail.com>
2024-08-27 17:01:33 +0300

    adding sorting bookmarks function

--- src/store/bookmarks.js
+++ src/store/bookmarks.js
@@ -18,9 +18,34 @@ export const useBookmarksStore = defineStore('bookmarksData', () => {
         }
     }
 
+    const sortedBookmarks = (sortName) => {
+        switch(sortName) {
+            case "yearASC":
+                bookmarks.value.sort((a, b) => a.year - b.year);
+                break;
+            case "yearDESC":
+                bookmarks.value.sort((a, b) => b.year - a.year);
+                break;
+            case "populASC":
+                bookmarks.value.sort((a, b) => a.rating.kp - b.rating.kp);
+                break;
+            case "populDESC":
+                bookmarks.value.sort((a, b) => b.rating.kp - a.rating.kp);
+                break;
+            case "lengthASC":
+                bookmarks.value.sort((a, b) => a.movieLength - b.movieLength);
+                break;
+            case "lengthDESC":
+                bookmarks.value.sort((a, b) => b.movieLength - a.movieLength);
+                break;
+            default:
+                return movies.value;
+        }
+    }
+
     watch(() => bookmarks, (state) => {
         localStorage.setItem("bookmarksData", JSON.stringify(state))
     }, { deep: true })
 
-    return {bookmarks, addMovieToBookmarks}
+    return {bookmarks, addMovieToBookmarks, sortedBookmarks}
 })
