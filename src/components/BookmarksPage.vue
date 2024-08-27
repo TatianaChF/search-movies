c5ce69b27f3dbcb170b7da59ed0493a2e6a12020
TatianaChF <tatiana.chefox.gmail.com>
2024-08-27 16:39:08 +0300

    display movie from bookmarks

---
+++ src/components/BookmarksPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container class="container">
+        <v-container 
+        class="container__card"
+        v-for="movie in bookmarksStore.bookmarks" 
+        :key="movie.id">
+          <movie-card :movieData="movie" />
+        </v-container>
+    </v-container>
+</template>
+
+<script setup>
+import { useBookmarksStore } from "../store/bookmarks";
+import MovieCard from "./MovieCard.vue";
+
+const bookmarksStore = useBookmarksStore();
+
+defineProps({
+    movieData: Object
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+
+  &__card {
+    flex: 250px;
+
+    &__router {
+      text-decoration: none;
+    }
+  }
+}
+
+.text {
+  text-align: center;
+}
+</style>
