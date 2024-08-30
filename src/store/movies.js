6072f04d441ec56257bdee1aa40faddccfd7dba2
TatianaChF <tatiana.chefox.gmail.com>
2024-08-30 12:28:30 +0300

    renamed variables

--- src/store/movies.js
+++ src/store/movies.js
@@ -5,6 +5,7 @@ import moviesData from "./../api/kinopoisk-1.json";
 
 export const useMoviesStore = defineStore('moviesData', () => {
     const movies = ref([]);
+    const sort = ref();
     const totalMovies = ref(100);
     const currentPage = ref(1);
     const pageSize = ref(25);
@@ -25,22 +26,22 @@ export const useMoviesStore = defineStore('moviesData', () => {
 
     const sortedMovies = (sortName) => {
         switch(sortName) {
-            case "yearASC":
+            case "году выпуска (по возрастанию)":
                 movies.value.sort((a, b) => a.year - b.year);
                 break;
-            case "yearDESC":
+            case "году выпуска (по убыванию)":
                 movies.value.sort((a, b) => b.year - a.year);
                 break;
-            case "populASC":
+            case "рейтингу (по возрастанию)":
                 movies.value.sort((a, b) => a.rating.kp - b.rating.kp);
                 break;
-            case "populDESC":
+            case "рейтингу (по убыванию)":
                 movies.value.sort((a, b) => b.rating.kp - a.rating.kp);
                 break;
-            case "lengthASC":
+            case "длительности (по возрастанию)":
                 movies.value.sort((a, b) => a.movieLength - b.movieLength);
                 break;
-            case "lengthDESC":
+            case "длительности (по убыванию)":
                 movies.value.sort((a, b) => b.movieLength - a.movieLength);
                 break;
             default:
