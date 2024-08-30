aee14b3e7e22b86f67dcec1b3a9b066086e3fc2c
TatianaChF <tatiana.chefox.gmail.com>
2024-08-30 12:48:11 +0300

    adding sort variable in store

--- src/store/movies.js
+++ src/store/movies.js
@@ -1,15 +1,20 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import axios from "axios";
 import moviesData from "./../api/kinopoisk-1.json";
 
 export const useMoviesStore = defineStore('moviesData', () => {
     const movies = ref([]);
-    const sort = ref();
     const totalMovies = ref(100);
     const currentPage = ref(1);
     const pageSize = ref(25);
     const lengthPagination = ref(0);
+    const currentSortValue = ref("нет сортировки");
+    const sortLocalStorage = localStorage.getItem("sortData");
+
+    watch(currentSortValue, () => {
+        console.log(currentSortValue.value)
+    })
 
     const getMovieData = async () => {
         const response = await axios.get(`http://localhost:3000/docs`);
@@ -28,21 +33,27 @@ export const useMoviesStore = defineStore('moviesData', () => {
         switch(sortName) {
             case "году выпуска (по возрастанию)":
                 movies.value.sort((a, b) => a.year - b.year);
+                currentSortValue.value = "году выпуска (по возрастанию)";
                 break;
             case "году выпуска (по убыванию)":
                 movies.value.sort((a, b) => b.year - a.year);
+                currentSortValue.value = "году выпуска (по убыванию)";
                 break;
             case "рейтингу (по возрастанию)":
                 movies.value.sort((a, b) => a.rating.kp - b.rating.kp);
+                currentSortValue.value = "рейтингу (по возрастанию)";
                 break;
             case "рейтингу (по убыванию)":
                 movies.value.sort((a, b) => b.rating.kp - a.rating.kp);
+                currentSortValue.value = "рейтингу (по убыванию)";
                 break;
             case "длительности (по возрастанию)":
                 movies.value.sort((a, b) => a.movieLength - b.movieLength);
+                currentSortValue.value = "длительности (по возрастанию)";
                 break;
             case "длительности (по убыванию)":
                 movies.value.sort((a, b) => b.movieLength - a.movieLength);
+                currentSortValue.value = "длительности (по убыванию)";
                 break;
             default:
                 return movies.value;
@@ -57,5 +68,5 @@ export const useMoviesStore = defineStore('moviesData', () => {
         })
     }
 
-    return {movies, displayedMovies, sortedMovies, searchMovie, getMovieData, currentPage, lengthPagination, pageSize}
+    return {movies, displayedMovies, currentSortValue, sortedMovies, searchMovie, getMovieData, currentPage, lengthPagination, pageSize}
 })
