962acee58b90605852bc2e095243593768e530f9
tikaya <tatiana.chefox@gmail.com>
2024-09-02 14:01:16 +0300

    update

--- src/components/SortingComponent.vue
+++ src/components/SortingComponent.vue
@@ -1,12 +1,17 @@
 <template>
   <v-container>
-    <b>Сортировать по: <span @click="isOpen = !isOpen" class="current-value">
-      {{ moviesStore.currentSortValue }}
-      </span> </b>
+    <b
+      >Сортировать по:
+      <span @click="isOpen = !isOpen" class="current-value">
+        {{ moviesStore.currentSortValue }}
+      </span>
+    </b>
     <v-container class="container-list" v-if="isOpen">
       <v-list width="300">
         <v-list-item v-for="sort in sortingValue" :key="sort">
-          <p @click="chooseSort(sort)" class="container-list__item">{{ sort }}</p>
+          <p @click="chooseSort(sort)" class="container-list__item">
+            {{ sort }}
+          </p>
         </v-list-item>
       </v-list>
     </v-container>
@@ -24,7 +29,7 @@ const sortingValue = [
   "рейтингу (по возрастанию)",
   "рейтингу (по убыванию)",
   "длительности (по возрастанию)",
-  "длительности (по убыванию)"
+  "длительности (по убыванию)",
 ];
 
 let isOpen = ref(false);
@@ -32,26 +37,26 @@ const moviesStore = useMoviesStore();
 const bookmarksStore = useBookmarksStore();
 
 const chooseSort = (sortValue) => {
-    moviesStore.currentSortValue = sortValue;
-    isOpen.value = false;
-    moviesStore.sortedMovies(sortValue);
-    bookmarksStore.sortedBookmarks(sortValue.name);
-}
+  moviesStore.currentSortValue = sortValue;
+  isOpen.value = false;
+  moviesStore.sortedMovies(sortValue);
+  bookmarksStore.sortedBookmarks(sortValue.name);
+};
 </script>
 
 <style lang="scss" scoped>
 .current-value {
-    text-decoration-line: underline;
-    cursor: pointer;
+  text-decoration-line: underline;
+  cursor: pointer;
 }
 
 .container-list {
-    position: absolute;
-    margin-left: 120px;
-    z-index: 2;
+  position: absolute;
+  margin-left: 120px;
+  z-index: 2;
 
-    &__item {
-        cursor: pointer;
-    }
+  &__item {
+    cursor: pointer;
+  }
 }
 </style>
