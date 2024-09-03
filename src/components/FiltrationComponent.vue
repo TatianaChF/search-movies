ca7ccf1c4efbd4188f0f29b824639c3385375e70
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 16:28:14 +0300

    adding range slider from vuetify

---
+++ src/components/FiltrationComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card>
+    <v-range-slider></v-range-slider>
+    <v-range-slider></v-range-slider>
+    <v-range-slider></v-range-slider>
+  </v-card>
+</template>
+
+<script setup></script>
