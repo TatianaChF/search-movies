a1944acfdad34e32be43f7a63582b3532749795e
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 16:30:37 +0300

    adding step for range slider

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card>
-    <v-range-slider></v-range-slider>
-    <v-range-slider></v-range-slider>
-    <v-range-slider></v-range-slider>
+    <v-range-slider :step="1"></v-range-slider>
+    <v-range-slider :step="1"></v-range-slider>
+    <v-range-slider :step="1"></v-range-slider>
   </v-card>
 </template>
 
