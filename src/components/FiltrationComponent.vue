c332aa96478bae2fa3dfa050e251ea4e88fc5f2e
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 16:46:31 +0300

    adding max-width for sliders

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card width="300">
     <v-slider max-width="100"></v-slider>
-    <v-slider></v-slider>
-    <v-slider></v-slider>
+    <v-slider max-width="100"></v-slider>
+    <v-slider max-width="100"></v-slider>
   </v-card>
 </template>
 
