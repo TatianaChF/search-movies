491c61d12d863e04981183b048a5e875827f5c69
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 16:43:31 +0300

    replace range sliders with sliders

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-card>
-    <v-range-slider :step="1"></v-range-slider>
-    <v-range-slider :step="1"></v-range-slider>
-    <v-range-slider :step="1"></v-range-slider>
+  <v-card width="300">
+    <v-slider :step="1" max-width="100"></v-slider>
+    <v-slider :step="1"></v-slider>
+    <v-slider :step="1"></v-slider>
   </v-card>
 </template>
 
