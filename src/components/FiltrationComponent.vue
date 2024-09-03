e677d171e452f152a8643eed5179e3aa2f177131
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 17:52:41 +0300

    adding text-field from vuetify

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -1,34 +1,65 @@
 <template>
-  <v-card width="200">
+  <v-card width="400">
     <v-container>
       <p>Год</p>
       <v-slider 
         v-model="year"
-        max-width="150" 
+        max-width="300" 
         :max="maxYear" 
         :min="minYear" 
         step="1" 
-        thumb-label />
+        thumb-label>
+          <template v-slot:append>
+            <v-text-field 
+              v-model="year"
+              density="compact"
+              style="width: 90px"
+              type="number"
+              hide-details
+              single-line />
+          </template>
+        </v-slider>
     </v-container>
     <v-container>
       <p>Рейтинг</p>
       <v-slider 
         v-model="rating"
-        max-width="150" 
+        max-width="300" 
         :max="maxRating" 
         :min="minRating"  
-        thumb-label />
+        thumb-label>
+          <template v-slot:append>
+            <v-text-field 
+              v-model="rating"
+              density="compact"
+              style="width: 90px"
+              type="number"
+              hide-details
+              single-line />
+          </template>
+        </v-slider>
     </v-container>
     <v-container>
       <p>Хронометраж</p>
       <v-slider 
         v-model="lengthMovie"
-        max-width="150" 
+        max-width="300" 
         :max="maxLength" 
         :min="minLength"  
         step="1" 
-        thumb-label />
+        thumb-label>
+          <template v-slot:append>
+            <v-text-field 
+              v-model="lengthMovie"
+              density="compact"
+              style="width: 90px"
+              type="number"
+              hide-details
+              single-line />
+          </template>
+        </v-slider>
     </v-container>
+    
   </v-card>
 </template>
 
