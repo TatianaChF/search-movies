045feda821c18a0a38084d466b327d11d46267c5
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 17:09:14 +0300

    definition max and min reatings

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -8,25 +8,36 @@
       :min="minYear" 
       step="1" 
       thumb-label />
-    <p></p>
-    <v-slider max-width="100"></v-slider>
+    <p>Рейтинг</p>
+    <v-slider 
+      v-model="rating"
+      max-width="150" 
+      :max="maxRating" 
+      :min="minRating"  
+      thumb-label />
     <v-slider max-width="100"></v-slider>
   </v-card>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { useMoviesStore } from "../store/movies";
 
 const moviesStore = useMoviesStore();
 const yearsMovies = [];
+const ratingsMovies = [];
 
 for (let i = 0; i < moviesStore.movies.length; i++) {
     yearsMovies.push(moviesStore.movies[i].year);
+    ratingsMovies.push(moviesStore.movies[i].rating.kp);
 }
 
 const maxYear = Math.max(...yearsMovies);
 const minYear = Math.min(...yearsMovies);
 const year = ref(maxYear);
+const maxRating = Math.max(...ratingsMovies);
+const minRating = Math.min(...ratingsMovies);
+const rating = ref(maxRating);
 
-console.log(minYear);
+console.log(rating);
 </script>
