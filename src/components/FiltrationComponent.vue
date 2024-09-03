80a5deacc339dd7827e77f86c1165e6ffb389f41
TatianaChF <tatiana.chefox.gmail.com>
2024-09-03 17:00:39 +0300

    definition max and min years, adding atep and thumb-label

--- src/components/FiltrationComponent.vue
+++ src/components/FiltrationComponent.vue
@@ -1,9 +1,29 @@
 <template>
   <v-card width="300">
-    <v-slider max-width="100"></v-slider>
+    <p>Год</p>
+    <v-slider 
+      max-width="150" 
+      :max="maxYear" 
+      :min="minYear" 
+      step="1" 
+      thumb-label />
     <v-slider max-width="100"></v-slider>
     <v-slider max-width="100"></v-slider>
   </v-card>
 </template>
 
-<script setup></script>
+<script setup>
+import { useMoviesStore } from "../store/movies";
+
+const moviesStore = useMoviesStore();
+const yearsMovies = [];
+
+for (let i = 0; i < moviesStore.movies.length; i++) {
+    yearsMovies.push(moviesStore.movies[i].year);
+}
+
+const maxYear = Math.max(...yearsMovies);
+const minYear = Math.min(...yearsMovies);
+
+console.log(minYear);
+</script>
