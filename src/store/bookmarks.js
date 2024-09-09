07c28782cf21d5e453b5aabb4712c88c07f24a33
tikaya <tatiana.chefox@gmail.com>
2024-09-09 15:40:28 +0300

    replace bookmarks with filteredBookmarks

--- src/store/bookmarks.js
+++ src/store/bookmarks.js
@@ -33,25 +33,25 @@ export const useBookmarksStore = defineStore('bookmarksData', () => {
     const sortedBookmarks = (sortName) => {
         switch(sortName) {
             case "году выпуска (по возрастанию)":
-                bookmarks.value.sort((a, b) => a.year - b.year);
+                filteredBookmarks.value.sort((a, b) => a.year - b.year);
                 break;
             case "году выпуска (по убыванию)":
-                bookmarks.value.sort((a, b) => b.year - a.year);
+                filteredBookmarks.value.sort((a, b) => b.year - a.year);
                 break;
             case "рейтингу (по возрастанию)":
-                bookmarks.value.sort((a, b) => a.rating.kp - b.rating.kp);
+                filteredBookmarks.value.sort((a, b) => a.rating.kp - b.rating.kp);
                 break;
             case "рейтингу (по убыванию)":
-                bookmarks.value.sort((a, b) => b.rating.kp - a.rating.kp);
+                filteredBookmarks.value.sort((a, b) => b.rating.kp - a.rating.kp);
                 break;
             case "длительности (по возрастанию)":
-                bookmarks.value.sort((a, b) => a.movieLength - b.movieLength);
+                filteredBookmarks.value.sort((a, b) => a.movieLength - b.movieLength);
                 break;
             case "длительности (по убыванию)":
-                bookmarks.value.sort((a, b) => b.movieLength - a.movieLength);
+                filteredBookmarks.value.sort((a, b) => b.movieLength - a.movieLength);
                 break;
             default:
-                return bookmarks.value;
+                return filteredBookmarks.value;
         }
     }
 
