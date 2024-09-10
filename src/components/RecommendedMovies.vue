15ef369026c2515c298cbf94ae1345673c1ea342
TatianaChF <tatiana.chefox.gmail.com>
2024-09-10 16:10:10 +0300

    display movie card

--- src/components/RecommendedMovies.vue
+++ src/components/RecommendedMovies.vue
@@ -2,17 +2,19 @@
   <v-container v-if="recommendedMovies.length > 0">
     <h3>Рекоммендуем посмотреть</h3>
     <v-container v-for="movie in recommendedMovies" :key="movie">
-        {{movie.name}}
+        <movie-card :movieData="movie" />
     </v-container>
   </v-container>
 </template>
 
 <script setup>
 import { useMoviesStore } from "../store/movies";
+import MovieCard from "./MovieCard.vue";
 
-const props = defineProps(["movie"]);
+const props = defineProps(["movie", "movieData"]);
 const moviesStore = useMoviesStore();
 const recommendedMovies = [];
+
 
 // цикл поиска альтернативных фильмов
 for (let i = 0; i < moviesStore.movies.length; i++) {
