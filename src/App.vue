907bf5233267c8f04bfb36209a4bcbe1b5125f58
tikaya <tatiana.chefox@gmail.com>
2024-09-11 17:08:14 +0300

    saving theme color in local storage

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
 import { useTheme } from "vuetify";
 import { RouterView, useRouter, useRoute } from 'vue-router';
 import SearchMovie from "./components/SearchMovie.vue";
+import { watch } from "vue";
 
 const theme = useTheme();
 const router = useRouter();
 const route = useRoute();
+const themeLocalStorage = localStorage.getItem("themeData");
+
+if (themeLocalStorage) {
+  theme.global.name.value = JSON.parse(themeLocalStorage);
+}
 
 function toggleTheme() {
   theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
 }
+
+watch(() => theme, (state) => {
+  localStorage.setItem("themeData", JSON.stringify(state.name.value));
+}, { deep: true }) 
 
 </script>
 
